c2268982b803ae1e441beb0007f0fdce800ccd42
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-07-02 21:40:15 +0100

    Add project files.

---
+++ RegistrationPage/DB/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegistrationPage.DB
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ RegistrationPage/Models/CreateUser.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace RegistrationPage.Models
+{
+    public class CreateUser
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$", ErrorMessage = "least 8 characters")]
+        public string Password { get; set; }
+        [ScaffoldColumn(false)]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "PhoneNumber is required")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "DOB is required")]
+        public DateTime DOB { get; set; }
+    }
+}
